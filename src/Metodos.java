8827aebb21f519daee43f711ec321f1d16ae81ea
Joao <joaovitordecarvalho07@gmail.com>
2022-02-18 09:09:08 -0300

    tá indo

--- src/Metodos.java
+++ src/Metodos.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Metodos {
+
+    public static void Cadastrar() {
+        System.out.println("Quantos funcionários deseja cadastrar?");
+        Scanner func = new Scanner(System.in);
+        int numero = func.nextInt();
+        int i = 0;
+        while (i != numero) {
+            System.out.println("Insira o nome do funcionário que deseja cadastrar:");
+            String funcionario = func.nextLine();
+            Pessoa.funcionarios.add(i, funcionario);
+            i++;
+        }
+    }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Pessoa {
-    
+    public static ArrayList<String> funcionarios = new ArrayList<String>();
 }
